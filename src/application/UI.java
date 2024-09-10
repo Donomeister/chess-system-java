414fae9ede6c12be857f288842854fd9a414c60e
Luan <luan.donato10@gmail.com>
2024-09-10 14:29:09 -0300

    Handling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,8 +7,11 @@ import chess.ChessPosition;
 import chess.Color;
 import chess.pieces.Rook;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
 
@@ -40,8 +43,10 @@ public class UI {
         }
     }
 
-    public static void printMatch(ChessMatch match){
+    public static void printMatch(ChessMatch match, List<ChessPiece> capturedPiece ){
         printBoard(match.getPieces());
+        System.out.println();
+        printCapturedPieces(capturedPiece);
         System.out.println();
         System.out.println("Turn: " + match.getTurn());
         System.out.println("Waiting player " + match.getcurrentPlayer());
@@ -86,5 +91,19 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    private static void printCapturedPieces(List<ChessPiece> capturedPiece){
+        List<ChessPiece> white = capturedPiece.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = capturedPiece.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+
+        System.out.println("Captured pieces: ");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print("Black: ");
+        System.out.print(ANSI_BLACK);
+        System.out.print(Arrays.toString(black.toArray()));
+        System.out.println(ANSI_RESET);
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,17 +1,23 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.BoardGameException;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
+
 
     public ChessMatch(){
         board = new Board(8,8);
@@ -87,11 +93,16 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p,target);
+        if(capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup(){
@@ -101,5 +112,12 @@ public class ChessMatch {
         placeNewPiece('e', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e',2 , new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 4, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 6, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 4, new Rook(board, Color.BLACK));
+        placeNewPiece('e',8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 6, new King(board, Color.BLACK));
     }
 }
